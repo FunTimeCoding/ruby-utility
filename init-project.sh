3689f6897db49a3b5e801df98b23cebf125ab397
Alexander Reitzel <funtimecoding@gmail.com>
2015-08-04 20:53:28 +0200

    remove excess quotes, rename example-script to rs

--- init-project.sh
+++ init-project.sh
@@ -23,13 +23,13 @@ if [ "${OS}" = "Darwin" ]; then
     alias sed='gsed'
 fi
 
-sed -i -e "s/ep/${INITIALS}/g" bin/example-script spec/example_project_spec.rb
-sed -i -e "s/example_project/${UNDERSCORE}/g" bin/example-script spec/example_project_spec.rb example_project.gemspec
-sed -i -e "s/ExampleProject/${CAMEL}/g" bin/example-script spec/example_project_spec.rb lib/example_project.rb
+sed -i -e "s/ep/${INITIALS}/g" bin/rs spec/example_project_spec.rb
+sed -i -e "s/example_project/${UNDERSCORE}/g" bin/rs spec/example_project_spec.rb example_project.gemspec
+sed -i -e "s/ExampleProject/${CAMEL}/g" bin/rs spec/example_project_spec.rb lib/example_project.rb
 sed -i -e "s/example-project/${DASH}/g" example_project.gemspec
-git mv "lib/example_project" "lib/${UNDERSCORE}"
-git mv "spec/example_project_spec.rb" "spec/${UNDERSCORE}_spec.rb"
-git mv "lib/example_project.rb" "lib/${UNDERSCORE}.rb"
-git mv "example_project.gemspec" "${UNDERSCORE}.gemspec"
-git mv "bin/example-script" "bin/${INITIALS}"
+git mv lib/example_project "lib/${UNDERSCORE}"
+git mv spec/example_project_spec.rb "spec/${UNDERSCORE}_spec.rb"
+git mv lib/example_project.rb "lib/${UNDERSCORE}.rb"
+git mv example_project.gemspec "${UNDERSCORE}.gemspec"
+git mv bin/rs "bin/${INITIALS}"
 echo "Done. Files were edited and moved using git. Review those changes. You may also delete this script now."
